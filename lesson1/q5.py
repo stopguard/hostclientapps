c16f812c86f5cc3202b5f3060922d5705b55597f
stopguard <stopguard@bk.ru>
2022-02-19 14:05:09 +0500

    all answers is ready

--- lesson1/q5.py
+++ lesson1/q5.py
@@ -8,16 +8,29 @@ from subprocess import Popen, PIPE
 import chardet
 
 
-def ping_pong(sources: list, ping_count: str):
+def ping_pong(target: str) -> list:
+    """ping source and returns a list of result byte lines"""
     multiple_arg = '-n' if system().lower() == 'windows' else '-c'
-    args = ['ping', multiple_arg, ping_count]
-    for target in sources:
-        result = Popen([*args, target], stdout=PIPE)
-        for line in result:
-            result = chardet.detect(line)
-            pass
+    args = ['ping', multiple_arg, '4']
+    responses = Popen([*args, target], stdout=PIPE)
+    return list(responses.stdout)
 
 
+def convert_to_str(bytes_data: list) -> str:
+    """convert list of byte lines to string"""
+    str_line = ''
+    if bytes_data:
+        code_table = chardet.detect(bytes_data[0]).get('encoding', 'ascii')
+        print(f'detected encoding: {code_table}')
+        for bytes_line in bytes_data:
+            str_line += bytes_line.decode(code_table)
+    return str_line
 
 
-sources = ['yandex.ru', 'youtube.com']
+if __name__ == '__main__':
+    sources = ['yandex.ru', 'youtube.com']
+    result = ''
+    for server in sources:
+        data = ping_pong(server)
+        result += f'{convert_to_str(data)}\n'
+    print(result)
--- lesson1/q6.py
+++ lesson1/q6.py
@@ -3,3 +3,27 @@
     «сетевое программирование», «сокет», «декоратор».
 Задача: открыть этот файл БЕЗ ОШИБОК вне зависимости от того, в какой кодировке он был создан.
 """
+from random import choice
+
+from q5 import convert_to_str
+
+
+code_tables = ['855', '866', 'cp1251', 'iso8859_5', 'koi8_r', 'utf-8']
+path = 'test_file.txt'
+data = ['сетевое программирование',
+        'сокет',
+        'декоратор']
+
+# create file with random encoding
+encoding = choice(code_tables)
+print(f'choised encoding:  {encoding}')
+with open(path, 'w', encoding=encoding) as f:
+    writable_data = (f'{word}\n' for word in data)
+    f.writelines(writable_data)
+
+# read file
+with open(path, 'rb') as f:
+    file_data = f.readlines()
+
+# decode file data
+print(convert_to_str(file_data))
