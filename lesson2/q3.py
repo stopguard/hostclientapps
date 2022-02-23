034d7c1288cc390b153aa6571685ad8646e838b8
stopguard <stopguard@bk.ru>
2022-02-23 18:41:00 +0500

    all answers is ready

--- lesson2/q3.py
+++ lesson2/q3.py
@@ -16,26 +16,31 @@ from pprint import pprint
 import yaml
 
 
+# prepare data
 writable_data = {
     'list': [1, 2, 'asd'],
     'number': 100500,
     'no_ascii_values_dict': {1: '1Б', 2: '2Г', 3: '3Д'},
 }
 
+# prepare file path
 target_dir = 'q3_files'
 target_filename = 'data.yaml'
 target_file_path = os.path.join(os.getcwd(), target_dir, target_filename)
 
+# serialize to yaml
 with open(target_file_path, 'w', encoding='utf-8') as f:
     yaml.dump(writable_data, f, default_flow_style=False, allow_unicode=True)
 
 print('writable data:')
 pprint(writable_data)
 
+# read raw data
 with open(target_file_path, 'r', encoding='utf-8') as f:
     print(f'{"-" * 100}\nraw yaml data:\n{f.read()}')
 
+# read deserialized data
 with open(target_file_path, 'r', encoding='utf-8') as f:
-    print(f'{"-" * 100}\nloaded yaml data:')
+    print(f'{"-" * 100}\ndeserialized yaml data:')
     pprint(yaml.load(f, Loader=yaml.FullLoader))
 
